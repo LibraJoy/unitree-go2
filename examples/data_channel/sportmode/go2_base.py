6d32a7e36b62cabb7acb9cf85ce51e8247bf9a0d
LibraJoy <144167578+LibraJoy@users.noreply.github.com>
2024-10-19 17:48:05 -0400

    Update go2_base.py

--- examples/data_channel/sportmode/go2_base.py
+++ examples/data_channel/sportmode/go2_base.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import json
 import numpy as np
+import math
 from scipy.spatial.transform import Rotation as R
 
 from go2_webrtc_driver.webrtc_driver import Go2WebRTCConnection, WebRTCConnectionMethod
@@ -17,83 +18,99 @@ import signal
 # Enable logging for debugging
 logging.basicConfig(level=logging.FATAL)
 
-# goal_position = [0.0, 0.0, 0.0]
-
-
-
 class go2_base:
     def __init__(self):
         self.odom_pub = rospy.Publisher('/go2_odom', Odometry, queue_size=10)
-        rospy.Subscriber('navigation/waypoints', PoseStamped, self.waypoint_callback)
+        rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.goal_pos_click_callback)
         self.current_state = None
         self.goal_pos = [0.0, 0.0, 0.0] # x, y, yaw
-        self.reach_tolerance = 0.2
+        self.reach_tolerance = 0.1
         self.new_waypoint_received = False
-
-        # Connect to the WebRTC service
-        self.conn = Go2WebRTCConnection(WebRTCConnectionMethod.Remote, serialNumber="B42D4000O1GBDIG2", username="fy.ya20026@gmail.com", password="Ykk1234?")
-        self.conn.connect()
-        self.conn.datachannel.pub_sub.subscribe(RTC_TOPIC['LF_SPORT_MOD_STATE'], self.sportmodestatus_callback)
-
-        # initialize motion
-        self.init_motion()
 
         # start odom thread
         self.odom_thread = threading.Thread(target=self.publish_odometry)
         self.odom_thread.daemon = True  # Daemon thread to exit when the main program exits
         self.odom_thread.start()
 
-        # move server
+    async def async_init(self):
+    # Connect to the WebRTC service
+        self.conn = Go2WebRTCConnection(WebRTCConnectionMethod.Remote, serialNumber="B42D4000O1GBDIG2", username="fy.ya20026@gmail.com", password="Ykk1234?")
+        await self.conn.connect()
+        self.conn.datachannel.pub_sub.subscribe(RTC_TOPIC['LF_SPORT_MOD_STATE'], self.sportmodestatus_callback)
 
-    def sportmodestatus_callback(self, message):  
+        # initialize motion
+        await self.init_motion()
+
+    def sportmodestatus_callback(self, message):
         self.current_state = message['data']
 
     def publish_odometry(self):
-        if self.current_state is None:
-            return  # Avoid running if current_state is not set yet
+        while not rospy.is_shutdown():
+            if self.current_state is None:
+                continue  # Avoid running if current_state is not set yet
 
-        odom_msg = Odometry()
-        position = self.current_state['position']
-        
-        # Set position data
-        odom_msg.pose.pose.position = Point(position[0], position[1], position[2])
-        
-        # Example: converting yaw from IMU to quaternion
-        yaw = self.current_state['imu_state']['rpy'][2]  # Roll-Pitch-Yaw from IMU
-        q = quaternion_from_euler(0, 0, yaw)
-        odom_msg.pose.pose.orientation = Quaternion(*q)
-        odom_msg.header.frame_id = 'map'
-        
-        # Publish odometry message to ROS
-        self.odom_pub.publish(odom_msg)
+            odom_msg = Odometry()
+            position = self.current_state['position']
+
+            # Set position data
+            odom_msg.pose.pose.position = Point(position[0], position[1], position[2])
+            yaw = self.current_state['imu_state']['rpy'][2]  # Roll-Pitch-Yaw from IMU
+            q = quaternion_from_euler(0, 0, yaw)
+            odom_msg.pose.pose.orientation = Quaternion(*q)
+            odom_msg.header.frame_id = 'map'
+
+            # Publish odometry message to ROS
+            self.odom_pub.publish(odom_msg)
+            rospy.sleep(0.1)  # Sleep for a short duration to avoid busy-waiting
 
     def waypoint_callback(self, msg):
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        z = msg.pose.position.z
+
+        self.goal_pos = [x, y, 0]
+        self.move_server()
+
+    def goal_pos_click_callback(self, msg):
         xgoal = msg.pose.position.x
         ygoal = msg.pose.position.y
         zgoal = msg.pose.position.z
 
-        x = msg.pose.orientation.x
-        y = msg.pose.orientation.y
-        z = msg.pose.orientation.z
-        w = msg.pose.orientation.w
-
-        quat_norm = np.linalg.norm([x, y, z, w])
-        if quat_norm == 0:
-            yawgoal = self.current_state['imu_state']['rpy'][2]
-        else:
-            quat = R.from_quat([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
-            yawgoal = quat.as_euler('xyz')[2]
-
-        print(f"Received waypoint: x={xgoal}, y={ygoal}, z={zgoal}, yaw={yawgoal}")
         self.new_waypoint_received = True
 
-    def init_motion(self):
+        self.goal_pos = [xgoal, ygoal, 0]
+        self.move_server()
+
+    def get_desired_heading(self, dx, dy):
+        position, quaternion = self.get_location()
+        current_x = position.x
+        current_y = position.y
+        current_rot = R.from_quat([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
+        current_yaw = current_rot.as_euler('xyz', degrees=False)[2]
+
+        x = dx - current_x
+        y = dy - current_y
+        goal_yaw = math.atan2(y, x)
+        print(f"current yaw: {current_yaw}, goal yaw: {goal_yaw}")
+
+        diff = abs(goal_yaw - current_yaw)
+        if diff > math.pi:
+            diff = 2 * math.pi - diff
+        print(f"diff: {diff}")
+        if math.pi / 8 < diff < 3 * math.pi / 4:
+            dyaw = goal_yaw
+        else:
+            dyaw = current_yaw
+        print(f"desired heading: {dyaw}")
+        return current_yaw, current_x, current_y, dyaw
+
+    async def init_motion(self):
         ####### NORMAL MODE ########
         print("Checking current motion mode...")
 
         # Get the current motion_switcher status
         response = self.conn.datachannel.pub_sub.publish_request_new(
-            RTC_TOPIC["MOTION_SWITCHER"], 
+            RTC_TOPIC["MOTION_SWITCHER"],
             {"api_id": 1001}
         )
 
@@ -106,7 +123,7 @@ class go2_base:
         if current_motion_switcher_mode != "normal":
             print(f"Switching motion mode from {current_motion_switcher_mode} to 'normal'...")
             self.conn.datachannel.pub_sub.publish_request_new(
-                RTC_TOPIC["MOTION_SWITCHER"], 
+                RTC_TOPIC["MOTION_SWITCHER"],
                 {
                     "api_id": 1002,
                     "parameter": {"name": "normal"}
@@ -115,7 +132,7 @@ class go2_base:
 
         print("StandUp ...")
         self.conn.datachannel.pub_sub.publish_request_new(
-            RTC_TOPIC["SPORT_MOD"], 
+            RTC_TOPIC["SPORT_MOD"],
             {
                 #"api_id": SPORT_CMD["StandUp"] # StandUp is lock
                 "api_id": SPORT_CMD["RecoveryStand"] # StandUp is lock
@@ -131,11 +148,34 @@ class go2_base:
 
         print("Stop ... ")
         self.conn.datachannel.pub_sub.publish_request_new(
-            RTC_TOPIC["SPORT_MOD"], 
+            RTC_TOPIC["SPORT_MOD"],
             {
                 "api_id": SPORT_CMD["StopMove"],
             }
         )
+
+    def genTrajectory(self, goal_pos):
+        x0, y0, yaw = self.current_state['position'][0], self.current_state['position'][1], self.current_state['imu_state']['rpy'][2]
+        x1, y1 = goal_pos[0], goal_pos[1]
+        dir_ray = [x1 - x0, y1 - y0]
+        dir_ray_norm = np.linalg.norm(dir_ray)
+        dir_ray = [dir_ray[0] / dir_ray_norm, dir_ray[1] / dir_ray_norm]
+        dt = 0.1
+        sp = 0.5 # speed
+        trajectory = []
+        for i in range(30):
+            tt = dt * i # time from start
+            data = {
+                "t_from_start": tt,
+                "x": sp * dir_ray[0] * tt + x0,
+                "y": sp * dir_ray[1] * tt + y0,
+                "yaw": yaw,
+                "vx": dir_ray[0] * sp,
+                "vy": dir_ray[1] * sp,
+                "vyaw": 0.0
+            }
+            trajectory.append(data)
+        return trajectory
 
     def move_server(self):
         rate = rospy.Rate(30)
@@ -147,15 +187,11 @@ class go2_base:
             if self.current_state != None:
                 print("Start position ...")
                 print(self.current_state['position'])
-                imu_state = self.current_state['imu_state']
-                w,x,y,z = imu_state['quaternion']
-                r = R.from_quat([x,y,z,w])
-                print(r.as_euler('xyz'))
 
-            if abs(self.current_state['position'][0] - goal_pos[0]) < tol and abs(self.current_state['position'][1] - goal_pos[1]) < tol:
+            if abs(self.current_state['position'][0] - self.goal_pos[0]) < self.tol and abs(self.current_state['position'][1] - self.goal_pos[1]) < self.tol:
                 print("Reach goal, Stop ... ")
                 self.conn.datachannel.pub_sub.publish_request_new(
-                    RTC_TOPIC["SPORT_MOD"], 
+                    RTC_TOPIC["SPORT_MOD"],
                     {
                         "api_id": SPORT_CMD["StopMove"],
                     }
@@ -163,31 +199,23 @@ class go2_base:
                 break
             else:
                 print("Trajectory ...")
-                trajectory = genTrajectory(goal_pos)
-                # for i in range(len(trajectory)):
-                #     print(f"Trajectory point {i}: x={trajectory[i]['x']}, y={trajectory[i]['y']}, yaw={trajectory[i]['yaw']}, vx={trajectory[i]['vx']}, vy={trajectory[i]['vy']}, vyaw={trajectory[i]['vyaw']}")
+                trajectory = self.genTrajectory(self.goal_pos)
                 self.conn.datachannel.pub_sub.publish_request_new(
-                    RTC_TOPIC["SPORT_MOD"], 
+                    RTC_TOPIC["SPORT_MOD"],
                     {
                         "api_id": SPORT_CMD["TrajectoryFollow"],
                         "parameter": trajectory
                     }
                 )
 
-            # await asyncio.sleep(3)
             print("finish one loop")
             if self.current_state != None:
                 print("End position ...")
                 print(self.current_state['position'])
-                imu_state = self.current_state['imu_state']
-                w,x,y,z = imu_state['quaternion']
-                r = R.from_quat([x,y,z,w])
-                print(r.as_euler('xyz'))
-            # await asyncio.sleep(3)
-            
             rate.sleep()  # Maintain 10Hz loop
-
 
 if __name__ == "__main__":
     rospy.init_node('go2_robot_controller', anonymous=True)
+    loop = asyncio.get_event_loop()
     go2_base_agent = go2_base()
+    loop.run_until_complete(go2_base_agent.async_init())
